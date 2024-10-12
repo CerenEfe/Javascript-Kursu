7c58e29c75fc9231d5aea96325a82ba9d735e12a
Ceren <cerenefe532@gmail.com>
2024-10-12 06:59:16 +0300

    Değişken tipleri

--- script.js
+++ script.js
@@ -1,49 +1,49 @@
-//Değişkenler
-
 // "a"
 
-var age;
-console.log(age);
+// DERS 2 : DEĞİŞKENLER
 
-age = 20;
-console.log(age);
+// var age;
+// console.log(age);
 
-var fullname = "ceren efe";
-console.log(fullname);
+// age = 20;
+// console.log(age);
 
-var fullname = "alara efe";
-console.log(fullname);
+// var fullname = "ceren efe";
+// console.log(fullname);
+
+// var fullname = "alara efe";
+// console.log(fullname);
 
 // Değişken Tanımlama Kuralları
 
 // Sayısal ifade ile başlayamaz örneğin 1sayi olmaz sayi1 olur
 
-var sayi1;
-var _sayi2;
-var $sayi3;
+// var sayi1;
+// var _sayi2;
+// var $sayi3;
 
 // Komut isimleri ile tanımlama yapılmaz. var for ya da var switch gibi..
 
 // Birden fazla kelime
 
-var ad = "ceren";
-var soyad = "efe";
+// var ad = "ceren";
+// var soyad = "efe";
 
-var ad_soyad = "Ceren Efe";
-var adSoyad = "Ceren Efe";
+// var ad_soyad = "Ceren Efe";
+// var adSoyad = "Ceren Efe";
 
 // Case Sentive (Büyük küçük harf duyarlılığı)
 
-var firstName = "Ceren";
-var FirstName = "Efe";
+// var firstName = "Ceren";
+// var FirstName = "Efe";
 
-console.log(firstName);
-console.log(FirstName);
+// console.log(firstName);
+// console.log(FirstName);
 
 // var, let, const
 
-let city = "Kocaeli";
-console.log(city);
+// let city = "Kocaeli";
+// console.log(city);
 
 //var email = "abc@gmail.com";
 //console.log(email);
@@ -51,13 +51,64 @@ console.log(city);
 //email = "def@gmail.com";
 //console.log(email);
 
-const email = "def@gmail.com";
-console.log(email);
+// const email = "def@gmail.com";
+// console.log(email);
 
-email = "def@gmail.com";
-console.log(email);
+// email = "def@gmail.com";
+// console.log(email);
 
 // const değer sabit olsun ve hiç değişmesin diye var.
+
+
+// DERS 3 : DEĞİŞKEN TİPLERİ
+
+// 1- Primitive Types ----------------------
+
+// String
+
+let firstName = "Ceren";
+console.log(typeof firstName);
+
+// Number 
+
+let age = 20;
+console.log(typeof age);
+
+let money = 100.05;
+console.log(typeof money);
+
+//boolean
+
+let isActive = true;
+console.log(typeof isActive);
+
+// null
+
+let job = null;
+console.log(typeof job);
+
+
+// undefined (tanımlanmamış)
+
+let car;
+console.log(typeof car);
+
+
+// 2- Reference Types = Objects ----------------------
+
+//object
+// array
+
+let names = ['ceren', 'efe', 'ali'];
+console.log(typeof names);
+
+// function
+
+var calculateAge = function() {
+    return 0;
+};
+
+console.log(typeof calculateAge);
 
 
 
