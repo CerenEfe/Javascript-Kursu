bca43fe19c5b6f7e5ac9c9c164e84586a9abcdbb
Ceren <cerenefe532@gmail.com>
2024-10-12 06:38:43 +0300

    Değişkenler konusu

---
+++ script.js
@@ -0,0 +1,68 @@
+//Değişkenler
+
+// "a"
+
+var age;
+console.log(age);
+
+age = 20;
+console.log(age);
+
+var fullname = "ceren efe";
+console.log(fullname);
+
+var fullname = "alara efe";
+console.log(fullname);
+
+// Değişken Tanımlama Kuralları
+
+// Sayısal ifade ile başlayamaz örneğin 1sayi olmaz sayi1 olur
+
+var sayi1;
+var _sayi2;
+var $sayi3;
+
+// Komut isimleri ile tanımlama yapılmaz. var for ya da var switch gibi..
+
+// Birden fazla kelime
+
+var ad = "ceren";
+var soyad = "efe";
+
+var ad_soyad = "Ceren Efe";
+var adSoyad = "Ceren Efe";
+
+// Case Sentive (Büyük küçük harf duyarlılığı)
+
+var firstName = "Ceren";
+var FirstName = "Efe";
+
+console.log(firstName);
+console.log(FirstName);
+
+// var, let, const
+
+let city = "Kocaeli";
+console.log(city);
+
+//var email = "abc@gmail.com";
+//console.log(email);
+
+//email = "def@gmail.com";
+//console.log(email);
+
+const email = "def@gmail.com";
+console.log(email);
+
+email = "def@gmail.com";
+console.log(email);
+
+// const değer sabit olsun ve hiç değişmesin diye var.
+
+
+
+
+
+
+
+
