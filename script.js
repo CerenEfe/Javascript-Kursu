d6c63fff7b0672ac748f7088ce13f03306d5061c
Ceren <cerenefe532@gmail.com>
2024-10-12 07:28:06 +0300

    Değişkenlerde Tip Dönüşümü

--- script.js
+++ script.js
@@ -66,32 +66,32 @@
 
 // String
 
-let firstName = "Ceren";
-console.log(typeof firstName);
+// let firstName = "Ceren";
+// console.log(typeof firstName);
 
 // Number 
 
-let age = 20;
-console.log(typeof age);
+// let age = 20;
+// console.log(typeof age);
 
-let money = 100.05;
-console.log(typeof money);
+// let money = 100.05;
+// console.log(typeof money);
 
 //boolean
 
-let isActive = true;
-console.log(typeof isActive);
+// let isActive = true;
+// console.log(typeof isActive);
 
 // null
 
-let job = null;
-console.log(typeof job);
+// let job = null;
+// console.log(typeof job);
 
 
 // undefined (tanımlanmamış)
 
-let car;
-console.log(typeof car);
+// let car;
+// console.log(typeof car);
 
 
 // 2- Reference Types = Objects ----------------------
@@ -99,16 +99,103 @@ console.log(typeof car);
 //object
 // array
 
-let names = ['ceren', 'efe', 'ali'];
-console.log(typeof names);
+// let names = ['ceren', 'efe', 'ali'];
+// console.log(typeof names);
 
-// function
+// // function
 
-var calculateAge = function() {
-    return 0;
-};
+// var calculateAge = function() {
+//     return 0;
+// };
 
-console.log(typeof calculateAge);
+// console.log(typeof calculateAge);
+
+// DERS 4 : TÜR DÖNÜŞÜMÜ
+
+// örnek 1 --------
+
+let num1 = '5';
+console.log(typeof num1);
+
+let num2 = '5';
+let num3 = '10';
+
+console.log(num2 + num3);
+
+// örnek 2 --------
+
+let sayi1 = Number('2');
+let sayi2 = Number('2');
+
+let total = sayi1+sayi2;
+
+console.log(total);
+console.log(typeof total);
+
+// Number to string
+
+let val;
+val = String(100);
+
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
+
+// arry to string
+
+let abc;
+abc = String([1,2,3,4]);
+
+console.log(abc);
+console.log(typeof abc);
+console.log(abc.length);
+
+//toString
+
+val = (10).toString();
+
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
+
+//string to number
+
+val = Number('10');
+
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
+//length sadece stringlere özgü 
+
+//parseInt
+//parseFloat
+
+val = parseInt('10');          // örnek 1 -----
+
+console.log(val);
+console.log(typeof val);
+console.log(val.toFixed(2));
+
+val = parseInt('10.5');        // örnek 1 -----
+
+console.log(val);
+console.log(typeof val);
+console.log(val.toFixed(2));
+
+val = parseFloat('10.5');      // örnek 1 -----
+
+console.log(val);
+console.log(typeof val);
+console.log(val.toFixed(2));
+
+
+
+
+
+
+
+
+
 
 
 
