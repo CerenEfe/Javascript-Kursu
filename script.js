43898f607875418e8b6f85c75b293702caebe88e
Ceren <cerenefe532@gmail.com>
2024-10-20 21:43:54 +0300

    döngüler uygulamalı örnek

--- script.js
+++ script.js
@@ -615,5 +615,29 @@
 // }while(i<10)
 
 
+// DERS 17 : DÖNGÜLER UYGULAMALI ÖRNEK +++++++++++++++++++++++++++++++++++++++++++++++++
 
 
+var hak = 5;
+var tahmin;
+var sayi = Math.floor((Math.random()*10)+1);
+
+console.log(sayi);
+
+while(hak>0){
+    hak--;
+    tahmin = Number(prompt('bir sayı giriniz'));
+
+    if(sayi == tahmin){
+        console.log('tebrikler bildiniz');
+        break;
+    }else if(sayi > tahmin){
+        console.log('yukarı');
+    }else{
+        console.log('aşağı');
+    }
+
+    if(hak == 0){
+        console.log('hakkınız bitti');
+    }
+}
