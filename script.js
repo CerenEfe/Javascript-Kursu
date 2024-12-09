f2bef1fdea487545f528f32aabc15369973e6187
Ceren <cerenefe532@gmail.com>
2024-12-09 03:07:25 +0300

    Dom Elementlerini Silme

--- script.js
+++ script.js
@@ -856,34 +856,88 @@
 
 // creat element
 
-const li = document.createElement('li');
+// const li = document.createElement('li');
 
-// add class
-li.className ='list-group-item list-group-item-secondary';
+// // add class
+// li.className ='list-group-item list-group-item-secondary';
 
-// attribute
-li.setAttribute('title','new title');
-li.setAttribute('data-id','5');
+// // attribute
+// li.setAttribute('title','new title');
+// li.setAttribute('data-id','5');
 
-// text node
-const text = document.createTextNode('new item');
-li.appendChild(text);
+// // text node
+// const text = document.createTextNode('new item');
+// li.appendChild(text);
 
-const a = document.createElement('a');
-a.setAttribute('href','#');
-a.className='delete-item float-right';
-a.innerHTML ='<i class="fas fa-times"></i>';
+// const a = document.createElement('a');
+// a.setAttribute('href','#');
+// a.className='delete-item float-right';
+// a.innerHTML ='<i class="fas fa-times"></i>';
 
-li.appendChild(a);  //append a to li
+// li.appendChild(a);  //append a to li
 
-// append li to ul
-document.querySelector('#task-list').appendChild(li);
+// // append li to ul
+// document.querySelector('#task-list').appendChild(li);
 
-//document.getElementById('task-list').appendChild(li);
+// //document.getElementById('task-list').appendChild(li);
 
-console.log(li);
+// console.log(li);
+
+
+// DERS 25 : DOM ELEMENTLERİNİ SİLME +++++++++++++++++++++++++++++++++++++++++++++++++
+
+const taskList = document.querySelector('#task-list');
+
+// ** removing element
+// taskList.remove();
+// taskList.childNodes[1].remove();
+// taskList.children[0].remove();
+
+// removing attribute
+// taskList.children[1].removeAttribute('class');
+
+// for(let i=0;i<taskList.children.length;i++){
+//     taskList.children[i].removeAttribute('class');
+// }
+
+
+// ** replacing elements
+// const cardHeader = document.querySelector('.card-header');
+
+// // create element
+// const h2 = document.createElement('h2');
+// h2.setAttribute('class','card-header');
+// h2.appendChild(document.createTextNode('My List'));
+
+// const parent = document.querySelector('.card');
+// parent.replaceChild(h2,cardHeader);
+
+// console.log(cardHeader);
+
+// ** Classes
+
+let val;
+
+link = taskList.children[0].children[0];
+
+// val = link.className;
+// val = link.classList;
+// val = link.classList[0];
+// val = link.classList[1];
+
+link.classList.add('new');
+link.classList.remove('new');
+
+// Attribues
+val = link.getAttribute('data-id');
+val = link.getAttribute('href');
+val = link.setAttribute('href','http://cerenefe.com');
+val = link.hasAttribute('data-val');
 
 
 
+
+
+console.log(val);
 
 
